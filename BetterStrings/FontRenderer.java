22aa540b6253ad0063d8b6b13caa78dddf48d352
Oppyusa <30739038+Oppyusa@users.noreply.github.com>
2021-01-11 14:34:16 -0500

    Update FontRenderer.java

--- BetterStrings/FontRenderer.java
+++ BetterStrings/FontRenderer.java
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) [year] [fullname]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.texclient.gui.util;
 
 import com.google.common.base.Splitter;
@@ -12,7 +36,10 @@ import java.awt.*;
 
 import static org.lwjgl.opengl.GL11.*;
 
-public class FontRenderer {
+/**
+ * @author Oppyusa
+ */
+public class TexFontRenderer {
 
 private Minecraft mc = Minecraft.getMinecraft();
 
@@ -24,11 +51,11 @@ private String fontName;
 
 private float kerning;
 
-public FontRenderer(String fontName, int fontType, int size) {
+public TexFontRenderer(String fontName, int fontType, int size) {
         this(fontName, fontType, size, 0);
 }
 
-public FontRenderer(String fontName, int fontType, int size, float kerning) {
+public TexFontRenderer(String fontName, int fontType, int size, float kerning) {
     this.fontName = fontName;
     this.fontType = fontType;
     this.size = size;
